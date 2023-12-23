e216d1195719b665d76eaa3e60e172dd4954ff80
Durk90 <derickryan90@gmail.com>
2023-12-23 20:35:15 +0000

    security

--- src/main/java/com/project/PropertyVersatile/PropertyVersatileApplication.java
+++ src/main/java/com/project/PropertyVersatile/PropertyVersatileApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.project.PropertyVersatile")
-@ComponentScan(basePackages = "com.project.PropertyVersatile") // Specify your base package
-@EntityScan(basePackages = "com.project.PropertyVersatile.entity") // Specify the package containing your entities
+@ComponentScan(basePackages = "com.project.PropertyVersatile")
+@EntityScan(basePackages = "com.project.PropertyVersatile.entity")
 public class PropertyVersatileApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PropertyVersatileApplication.class, args);
     }
-
 }
