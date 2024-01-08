62d82f8c14218dda17441250c39ee05058147034
Durk90 <derickryan90@gmail.com>
2024-01-08 15:15:51 +0000

    App

--- src/main/java/com/project/PropertyVersatile/PropertyVersatileApplication.java
+++ src/main/java/com/project/PropertyVersatile/PropertyVersatileApplication.java
@@ -3,10 +3,8 @@ package com.project.PropertyVersatile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.project.PropertyVersatile")
-@ComponentScan(basePackages = "com.project.PropertyVersatile")
+@SpringBootApplication
 @EntityScan(basePackages = "com.project.PropertyVersatile.entity")
 public class PropertyVersatileApplication {
 
