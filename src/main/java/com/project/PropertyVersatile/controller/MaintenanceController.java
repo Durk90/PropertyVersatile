57a0c554931f6d5c693fc7cd44ce284a06213470
Durk90 <derickryan90@gmail.com>
2024-01-08 18:00:23 +0000

    Maintenance

--- src/main/java/com/project/PropertyVersatile/controller/MaintenanceController.java
+++ src/main/java/com/project/PropertyVersatile/controller/MaintenanceController.java
@@ -36,8 +36,9 @@ public class MaintenanceController {
     @GetMapping("/create")
     public String showCreateMaintenanceForm(Model model) {
         model.addAttribute("maintenance", new Maintenance());
-        return "maintenance";
+        return "create-maintenance"; // Change the view name to "create-maintenance"
     }
+
 
     @PostMapping("/create")
     public String createMaintenance(@ModelAttribute Maintenance maintenance, Model model) {
