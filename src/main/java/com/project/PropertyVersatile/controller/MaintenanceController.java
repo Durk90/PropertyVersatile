77e5e803eacb85e68438f8b8d6779a785e332290
Durk90 <derickryan90@gmail.com>
2024-01-08 19:05:28 +0000

    Maintenance

--- src/main/java/com/project/PropertyVersatile/controller/MaintenanceController.java
+++ src/main/java/com/project/PropertyVersatile/controller/MaintenanceController.java
@@ -59,7 +59,7 @@ public class MaintenanceController {
             Maintenance maintenance = maintenanceService.getMaintenanceById(maintenanceId);
             model.addAttribute("maintenance", maintenance);
             model.addAttribute("action", "edit"); // Set action to 'edit'
-            return "edit-maintenance";
+            return "maintenance";
         } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("error", "Error retrieving maintenance details for editing");
