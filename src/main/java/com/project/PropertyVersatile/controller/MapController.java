04321c0071e54f36b16438ae797aa2e26776a057
Durk90 <derickryan90@gmail.com>
2024-01-09 15:10:08 +0000

    Map

---
+++ src/main/java/com/project/PropertyVersatile/controller/MapController.java
@@ -0,0 +1,18 @@
+package com.project.PropertyVersatile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/maintenance-requests")
+    public String showMaintenanceRequestsMap(Model model) {
+       
+        model.addAttribute("mapMessage", "Welcome to the Maintenance Requests Map!");
+
+        // Return the Thymeleaf template name (maintenance-requests.html)
+        return "engineers";
+    }
+}
