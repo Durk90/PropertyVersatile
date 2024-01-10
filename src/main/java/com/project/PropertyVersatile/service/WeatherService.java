316b8e3f601722ae13695e6190e6d21ff8d58a6d
Durk90 <derickryan90@gmail.com>
2024-01-10 00:25:28 +0000

    Weather

--- src/main/java/com/project/PropertyVersatile/service/WeatherService.java
+++ src/main/java/com/project/PropertyVersatile/service/WeatherService.java
@@ -3,22 +3,31 @@ package com.project.PropertyVersatile.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.project.PropertyVersatile.model.WeatherData;
 
 @Service
 public class WeatherService {
-    @Value("${api.url}")
-    private String apiUrl;
-
-    private final RestTemplate restTemplate;
-
-    public WeatherService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    
+    @Value("${weather.api.url}")
+    private String weatherApiUrl;
 
     public WeatherData getWeatherData(double latitude, double longitude) {
-        String url = apiUrl + "?latitude=" + latitude + "&longitude=" + longitude;
-        return restTemplate.getForObject(url, WeatherData.class);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(weatherApiUrl)
+                .queryParam("latitude", latitude)
+                .queryParam("longitude", longitude)
+                .queryParam("current", "temperature_2m,rain,showers,snowfall")
+                .queryParam("hourly", "temperature_2m,relative_humidity_2m,precipitation_probability,snowfall,snow_depth,wind_speed_10m")
+                .queryParam("timezone", "Europe/London");
+
+        String apiUrl = builder.toUriString();
+
+        // Make an HTTP request to the weather API
+        RestTemplate restTemplate = new RestTemplate();
+        WeatherData weatherData = restTemplate.getForObject(apiUrl, WeatherData.class);
+
+        return weatherData;
     }
 }
+
