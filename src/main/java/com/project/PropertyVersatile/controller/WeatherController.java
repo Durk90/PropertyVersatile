4cd34770eafb80d93b0161b6c59dad3c11567302
Durk90 <derickryan90@gmail.com>
2024-01-10 14:27:10 +0000

    Weather

--- src/main/java/com/project/PropertyVersatile/controller/WeatherController.java
+++ src/main/java/com/project/PropertyVersatile/controller/WeatherController.java
@@ -1,28 +1,18 @@
 package com.project.PropertyVersatile.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.project.PropertyVersatile.model.WeatherData;
-import com.project.PropertyVersatile.service.WeatherService;
 
 @Controller
 public class WeatherController {
-    private final WeatherService weatherService;
-
-    public WeatherController(WeatherService weatherService) {
-        this.weatherService = weatherService;
-    }
 
     @GetMapping("/weather")
-    public String getWeather(Model model) {
-        double latitude = 52.52;
-        double longitude = 13.41;
+    public String showForecastChart() {
+        return "weather"; // This corresponds to forecast.html
+    }
 
-        WeatherData weatherData = weatherService.getWeatherData(latitude, longitude);
-        model.addAttribute("weatherData", weatherData);
-
-        return "weather";
+    @GetMapping("/archive")
+    public String showArchiveChart() {
+        return "weather-archive"; // This corresponds to archive.html
     }
 }
