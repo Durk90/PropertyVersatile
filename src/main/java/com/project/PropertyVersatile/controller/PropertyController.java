e2e3872c05614174ad8ae9ace2c78b05b4d81f1c
Durk90 <derickryan90@gmail.com>
2024-01-13 15:34:24 +0000

    Forms

--- src/main/java/com/project/PropertyVersatile/controller/PropertyController.java
+++ src/main/java/com/project/PropertyVersatile/controller/PropertyController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/properties")
